65da7bd068ed5bf8de797b9bb3db32eebc35392f
Anthony <125139205@qq.com>
2019-12-13 22:52:21 +0800

    Execution Package

---
+++ Wuoab/src/Execution/User.java
@@ -0,0 +1,9 @@
+package Execution;
+
+public class User implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("233");
+    }
+}
---
+++ Wuoab/src/Execution/WelcomToWuoab.java
@@ -0,0 +1,7 @@
+package Execution;
+
+public class WelcomToWuoab {
+    public static void main(String[] args) {
+        new Thread(new User()).start();
+    }
+}
